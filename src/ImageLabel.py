c6aa108c7e080ba7f6d2d7b35c9c3463e2a59e67
Janis <ignatov.vit@gmail.com>
2011-08-30 10:16:12 +0400

    Updated getting image from web

--- src/ImageLabel.py
+++ src/ImageLabel.py
@@ -1,10 +1,14 @@
-from PyQt4 import QtGui, QtCore, QtNetwork
+from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import Qt
 
 MAX_IMAGE_WIDTH = 1024
 MAX_IMAGE_HEIGHT = 1024
 
 class ImageLabel(QtGui.QLabel):
+    latestDir = QtCore.QDir.currentPath()
+    # TODO: Uncomment default location in release 
+    # latestDir = QtGui.QDesktopServices.storageLocation(QtGui.QDesktopServices.PicturesLocation)
+    
     def __init__(self, parent=None):
         super(ImageLabel, self).__init__(parent)
         
@@ -32,15 +36,12 @@ class ImageLabel(QtGui.QLabel):
         delete = QtGui.QAction(icon, 'Delete', self)
         delete.triggered.connect(self.deleteImage)
 
-        icon = style.standardIcon(QtGui.QStyle.SP_DriveFDIcon)
-        save = QtGui.QAction(icon, 'Save as...', self)
+        save = QtGui.QAction('Save as...', self)
         save.triggered.connect(self.saveImage)
-
-        edit = QtGui.QAction('Edit...', self)
-        edit.triggered.connect(self.editImage)
 
         menu = QtGui.QMenu()
         menu.addAction(open)
+        menu.setDefaultAction(open)
         menu.addAction(paste)
         menu.addAction(delete)
         if self.image.isNull():
@@ -48,57 +49,51 @@ class ImageLabel(QtGui.QLabel):
         menu.addAction(save)
         if self.image.isNull():
             save.setEnabled(False)
-        menu.addAction(edit)
-        if self.image.isNull():
-            edit.setEnabled(False)
         menu.exec_(self.mapToGlobal(pos))
+    
+    def mouseDoubleClickEvent(self, e):
+        self.openImage()
         
     def openImage(self):
         fileName = QtGui.QFileDialog.getOpenFileName(self,
-                "Open File", QtCore.QDir.currentPath(),
+                "Open File", ImageLabel.latestDir,
                 "Images (*.bmp *.png *.jpg *.jpeg *.tiff *.gif);;All files (*.*)")
         if fileName:
+            ImageLabel.latestDir = QtCore.QDir(fileName).absolutePath()
             self.loadFromFile(fileName)
 
     def deleteImage(self):
         self.__clearImage()
     
     def saveImage(self):
+        # TODO: Change latestDir to user's pictures dir
         fileName = QtGui.QFileDialog.getSaveFileName(self,
-                "Save File", QtCore.QDir.currentPath(),
+                "Save File", ImageLabel.latestDir,
                 "Images (*.bmp *.png *.jpg *.jpeg *.tiff *.gif);;All files (*.*)")
         if fileName:
+            ImageLabel.latestDir = QtCore.QDir(fileName).absolutePath()
             self.image.save(fileName)
     
     def pasteImage(self):
         mime = QtGui.QApplication.clipboard().mimeData()
         if mime.hasUrls():
-            self.image.load(mime.urls()[0])
+            fileName = mime.urls()[0]
+            self.image.load(fileName)
         elif mime.hasImage():
             self.image = mime.imageData()
         elif mime.hasText():
-            # http://blog.mrcongwang.com/2009/07/21/applying-system-proxy-settings-to-qt-application/
-            # http://www.erata.net/qt-boost/synchronous-http-request/
-            url = QtCore.QUrl("http://www.stockmusic.com/uploads/wmm01-p.jpg");
-            self.bytes = QtCore.QByteArray()
-            buffer = QtCore.QBuffer(self.bytes)
-            buffer.open(QtCore.QIODevice.WriteOnly);
-            http = QtNetwork.QHttp(self)
-            http.requestFinished.connect(self.requestFinished)
-            http.setHost(url.host());
-            self.Request=http.get(url.path(),buffer);
+            # Load image by URL
+            import urllib.request
+            url = mime.text()
+            try:
+                data = urllib.request.urlopen(url).read()
+                self.image.loadFromData(data)
+            except:
+                return
         else:
             return
 
         self.__setImage()
-    
-    def requestFinished(self, requestId, error):
-        print(requestId, self.Request, error)
-        if self.Request == requestId:
-            self.image = QtGui.QImage()
-            print(self.bytes)
-            self.image.loadFromData(self.bytes);
-            self.__setImage()
 
     def editImage(self):
         raise
@@ -129,26 +124,23 @@ class ImageLabel(QtGui.QLabel):
     
     def loadFromFile(self, fileName):
         self.image.load(fileName)
-        # Resize big images for storing in DB
-        if self.image.width() > MAX_IMAGE_WIDTH or self.image.height() > MAX_IMAGE_HEIGHT:
-            self.image = self.image.scaled(MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         self.__setImage()
     
     def loadFromData(self, data):
         self.image.loadFromData(data)
         self.__setImage()
         
-    def loadFromUrl(self, url):
-        raise
-    
-    def loadFromClipboard(self):
-        raise
-    
     def data(self):
         ba = QtCore.QByteArray() 
         buffer = QtCore.QBuffer(ba)
         buffer.open(QtCore.QIODevice.WriteOnly)
-        self.image.save(buffer, "PNG")
+
+        # Resize big images for storing in DB
+        if self.image.width() > MAX_IMAGE_WIDTH or self.image.height() > MAX_IMAGE_HEIGHT:
+            scaledImage = self.image.scaled(MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        else:
+            scaledImage = self.image
+        scaledImage.save(buffer, "PNG")
         return ba
 
 if __name__ == '__main__':
