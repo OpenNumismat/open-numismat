b740eedf65d5c3e7c81f7ff4e6bd8d84f8e3968a
Janis <ignatov.vit@gmail.com>
2011-09-03 20:33:14 +0400

    Moved form items to separate module

--- src/BaseFormLayout.py
+++ src/BaseFormLayout.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtGui
 from PyQt4.QtCore import Qt, QDate
 
+from FormItems import *
 from ImageLabel import ImageLabel
 
 class FormItemTypes():
@@ -14,85 +15,6 @@ class FormItemTypes():
     Image = 8
     Money = 9
     Value = 10
-
-class LineEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(LineEdit, self).__init__(parent)
-        self.setMaxLength(1024)
-
-class ShortLineEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(ShortLineEdit, self).__init__(parent)
-        self.setMaxLength(10)
-        self.setMinimumWidth(100)
-        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-    
-    def sizeHint(self):
-        return self.minimumSizeHint()
-
-class NumberEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(NumberEdit, self).__init__(parent)
-        validator = QtGui.QIntValidator(0, 9999, parent)
-        self.setValidator(validator)
-        self.setMaxLength(15)
-        self.setMinimumWidth(100)
-        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-
-    def sizeHint(self):
-        return self.minimumSizeHint()
-
-class BigIntEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(BigIntEdit, self).__init__(parent)
-        validator = QtGui.QDoubleValidator(0, 999999999999999, 0, parent)
-        validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
-        self.setValidator(validator)
-        self.setMaxLength(15)
-        self.setMinimumWidth(100)
-        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-
-class ValueEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(ValueEdit, self).__init__(parent)
-        validator = QtGui.QDoubleValidator(0, 9999999999, 3, parent)
-        validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
-        self.setValidator(validator)
-        self.setMaxLength(15)
-        self.setMinimumWidth(100)
-        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-    
-    def sizeHint(self):
-        return self.minimumSizeHint()
-
-class MoneyEdit(QtGui.QLineEdit):
-    def __init__(self, parent=None):
-        super(MoneyEdit, self).__init__(parent)
-        validator = QtGui.QDoubleValidator(0, 9999999999, 2, parent)
-        validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
-        self.setValidator(validator)
-        self.setMaxLength(15)
-        self.setMinimumWidth(100)
-        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-    
-    def sizeHint(self):
-        return self.minimumSizeHint()
-
-class TextEdit(QtGui.QTextEdit):
-    def __init__(self, parent=None):
-        super(TextEdit, self).__init__(parent)
-
-    def sizeHint(self):
-        return self.minimumSizeHint()
-
-class DateEdit(QtGui.QDateEdit):
-    def __init__(self, parent=None):
-        super(DateEdit, self).__init__(parent)
-        calendar = QtGui.QCalendarWidget()
-        calendar.setGridVisible(True)
-        self.setCalendarPopup(True)
-        self.setCalendarWidget(calendar)
-        self.setMinimumWidth(85)
 
 class FormItem(object):
     def __init__(self, field, title, itemType, parent=None):
