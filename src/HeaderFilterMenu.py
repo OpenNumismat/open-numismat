138cb4656dff96bb5c81fe7c9cbf06496cffa9d3
Janis <ignatov.vit@gmail.com>
2011-09-23 12:44:44 +0400

    Added Blanks and All items to filter

--- src/HeaderFilterMenu.py
+++ src/HeaderFilterMenu.py
@@ -3,6 +3,10 @@ from PyQt4.QtCore import Qt
 from PyQt4.QtSql import QSqlQuery
 
 class FilterMenuButton(QtGui.QPushButton):
+    DefaultType = 0
+    SelectAllType = 1
+    BlanksType = 2
+    
     def __init__(self, columnName, model, parent):
         super(FilterMenuButton, self).__init__(parent)
         
@@ -30,10 +34,27 @@ class FilterMenuButton(QtGui.QPushButton):
         sql = "SELECT DISTINCT %s FROM coins %s" % (self.columnName, filtersSql)
         query = QSqlQuery(sql, self.db)
 
+        item = QtGui.QListWidgetItem(self.tr("(Select all)"), self.listWidget, FilterMenuButton.SelectAllType)
+        item.setCheckState(Qt.PartiallyChecked)
+        self.listWidget.addItem(item)
+        hasBlanks = False
         while query.next():
-            item = QtGui.QListWidgetItem(str(query.record().value(0)), self.listWidget)
+            label = str(query.record().value(0))
+            if not label:
+                hasBlanks = True
+                continue
+            item = QtGui.QListWidgetItem(label, self.listWidget)
             item.setCheckState(Qt.Checked)
             self.listWidget.addItem(item)
+        
+        if hasBlanks:
+            item = QtGui.QListWidgetItem(self.tr("(Blanks)"), self.listWidget, FilterMenuButton.BlanksType)
+            item.setCheckState(Qt.Checked)
+            self.listWidget.addItem(item)
+        
+        self.listWidget.itemChanged.connect(self.itemChanged)
+        
+        self.itemChanged(item)
         
         buttonBox = QtGui.QDialogButtonBox(Qt.Horizontal)
         buttonBox.addButton(QtGui.QDialogButtonBox.Ok)
@@ -52,13 +73,39 @@ class FilterMenuButton(QtGui.QPushButton):
         widgetAction.setDefaultWidget(widget)
         self.menu().clear()
         self.menu().addAction(widgetAction)
+    
+    def itemChanged(self, item):
+        self.listWidget.itemChanged.disconnect(self.itemChanged)
+
+        if item.type() == FilterMenuButton.SelectAllType:
+            for i in range(1, self.listWidget.count()):
+                self.listWidget.item(i).setCheckState(item.checkState())
+        else:
+            checkedCount = 0
+            for i in range(1, self.listWidget.count()):
+                item = self.listWidget.item(i)
+                if item.checkState() == Qt.Checked:
+                    checkedCount = checkedCount + 1
+
+            if checkedCount == 0:
+                state = Qt.Unchecked
+            elif checkedCount == self.listWidget.count()-1:
+                state = Qt.Checked
+            else:
+                state = Qt.PartiallyChecked
+            self.listWidget.item(0).setCheckState(state)
+
+        self.listWidget.itemChanged.connect(self.itemChanged)
 
     def apply(self):
         filters = []
-        for i in range(self.listWidget.count()):
+        for i in range(1, self.listWidget.count()):
             item = self.listWidget.item(i)
             if item.checkState() == Qt.Unchecked:
-                filters.append("%s<>'%s'" % (self.columnName, item.text()))
+                if item.type() == FilterMenuButton.DefaultType:
+                    filters.append("%s<>'%s'" % (self.columnName, item.text()))
+                else:
+                    filters.append("%s<>''" % self.columnName)
 
         if filters:
             filterSql = ' AND '.join(filters)
