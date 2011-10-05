4041667f16d15a5b896a5365088f2011fed97ca7
Janis <ignatov.vit@gmail.com>
2011-10-05 15:02:19 +0400

    Added buttons for images

--- src/PageView.py
+++ src/PageView.py
@@ -1,7 +1,9 @@
 from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import Qt
 
 from ListView import ListView
 from EditCoinDialog.ImageLabel import ImageLabel
+from Collection.CollectionFields import FieldTypes as Type
 
 class PageView(QtGui.QSplitter):
     def __init__(self, listParam, parent=None):
@@ -9,20 +11,40 @@ class PageView(QtGui.QSplitter):
         
         self.tableView = ListView(listParam, self)
         self.tableView.rowChanged.connect(self.rowChanged)
-        
-        self.layout = QtGui.QVBoxLayout()
-        self.layout.setContentsMargins(QtCore.QMargins())
-        widget = QtGui.QWidget(self)
-        widget.resize(120, 0)
-        widget.setLayout(self.layout)
-        
         self.addWidget(self.tableView)
+        
+        layout = QtGui.QVBoxLayout(self)
+        
+        self.imageLayout = QtGui.QVBoxLayout()
+        self.imageLayout.setContentsMargins(QtCore.QMargins())
+        layout.addWidget(self.__layoutToWidget(self.imageLayout))
+        
+        self.buttonLayout = QtGui.QHBoxLayout()
+        self.buttonLayout.setAlignment(Qt.AlignCenter | Qt.AlignBottom)
+        widget = self.__layoutToWidget(self.buttonLayout)
+        widget.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        layout.addWidget(widget)
+        
+        widget = self.__layoutToWidget(layout)
+        widget.resize(120, 0)
         self.addWidget(widget)
         
         self.splitterMoved.connect(self.splitterPosChanged)
 
     def setModel(self, model):
         self.tableView.setModel(model)
+        
+        self.imageButtons = []
+        self.imageFields = []
+        for field in model.fields:
+            if field.type == Type.Image:
+                self.imageFields.append(field.name)
+                button = QtGui.QCheckBox(self)
+                button.setToolTip(field.title)
+                button.setDisabled(False)
+                button.stateChanged.connect(self.buttonClicked)
+                self.imageButtons.append(button)
+                self.buttonLayout.addWidget(button)
     
     def model(self):
         return self.tableView.model()
@@ -42,19 +64,48 @@ class PageView(QtGui.QSplitter):
             self.setSizes(sizes)
             self.splitterMoved.connect(self.splitterPosChanged)
     
-    def rowChanged(self, current):
-        for _ in range(self.layout.count()):
-            item = self.layout.itemAt(0)
-            item.widget().clear()
-            self.layout.removeItem(item)
+    def buttonClicked(self, state):
+        self.clear()
 
+        current = self.tableView.currentIndex()
+        for i, field in enumerate(self.imageFields):
+            if self.imageButtons[i].checkState() == Qt.Checked:
+                image = ImageLabel(self)
+                index = self.model().index(current.row(), self.model().fieldIndex(field))
+                image.loadFromData(index.data())
+                self.imageLayout.addWidget(image)
+    
+    def rowChanged(self, current):
+        self.clear()
+        
         images = []
-        for field in ['obverseimg', 'reverseimg']:
+        for i, field in enumerate(self.imageFields):
+            self.imageButtons[i].stateChanged.disconnect(self.buttonClicked)
+            self.imageButtons[i].setCheckState(Qt.Unchecked)
+            self.imageButtons[i].setDisabled(True)
+
             index = self.model().index(current.row(), self.model().fieldIndex(field))
-            if not index.data() or not index.data().isNull():
-                images.append(index.data())
+            if index.data() and not index.data().isNull():
+                # By default show only first 2 images
+                if len(images) < 2:
+                    images.append(index.data())
+                    self.imageButtons[i].setCheckState(Qt.Checked)
+                self.imageButtons[i].setDisabled(False)
+
+            self.imageButtons[i].stateChanged.connect(self.buttonClicked)
 
         for imageData in images:
             image = ImageLabel(self)
             image.loadFromData(imageData)
-            self.layout.addWidget(image)
+            self.imageLayout.addWidget(image)
+    
+    def clear(self):
+        for _ in range(self.imageLayout.count()):
+            item = self.imageLayout.itemAt(0)
+            item.widget().clear()
+            self.imageLayout.removeItem(item)
+
+    def __layoutToWidget(self, layout):
+        widget = QtGui.QWidget(self)
+        widget.setLayout(layout)
+        return widget
