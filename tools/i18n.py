4e2feccb7041efdec130f99fc95588f9dd1cdc36
Janis <ignatov.vit@gmail.com>
2011-10-19 14:00:13 +0400

    Added i18n tool

---
+++ tools/i18n.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+import PyQt4
+
+pyqtDir = os.path.dirname(PyQt4.__file__)
+lupdatePath = os.path.join(pyqtDir, 'pylupdate4.exe')
+linguistPath = os.path.join(pyqtDir, 'linguist.exe')
+    
+srcFiles = []
+for dirname, dirnames, filenames in os.walk('../src'):
+    for filename in filenames:
+        fileName, fileExtension = os.path.splitext(filename)
+        if fileExtension == '.py':
+            srcFiles.append(os.path.join(dirname, filename))
+    
+outputfile = 'lang_ru.ts'
+os.system(' '.join([lupdatePath, ' '.join(srcFiles), '-ts', outputfile]))
+os.system(' '.join([linguistPath, outputfile]))
