b312c5b24449f11c471cce156e7ee7822c464cd7
Janis <ignatov.vit@gmail.com>
2011-10-27 14:19:26 +0400

    Added copying dest files

--- tools/setup.py
+++ tools/setup.py
@@ -1,18 +1,27 @@
-import sys
-
+import os, shutil, sys
 from cx_Freeze import Executable, Freezer
 
 sys.path.append('../src')
 import version
 
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
+
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-executables = [Executable("../src/main.py", base=base, targetName=version.AppName+".exe")]
+executables = [Executable("../src/main.py", targetName=version.AppName+".exe")]
 freezer = Freezer(executables,
-            includes = ["lxml._elementpath", "gzip", "inspect"],
-            excludes = ["win32api", "win32pipe", "win32con"],
-            compress = True,
-            base = base)
+    includes = ["lxml._elementpath", "gzip", "inspect"],
+    excludes = ["win32api", "win32pipe", "win32con"],
+    compress = True,
+    base = base)
 freezer.Freeze()
+
+shutil.copytree("../src/icons", "dist/icons")
+shutil.copy("../src/lang_ru.qm", "dist")
+import PyQt4
+shutil.copytree(PyQt4.__path__[0]+"/plugins/imageformats", "dist/imageformats")
+os.mkdir("dist/sqldrivers")
+shutil.copy(PyQt4.__path__[0]+"/plugins/sqldrivers/qsqlite4.dll", "dist/sqldrivers")
