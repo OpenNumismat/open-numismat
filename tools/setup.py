252ff9f670e654cfb5edc5ee4843c7da1d80d4e2
Janis <ignatov.vit@gmail.com>
2011-10-28 10:15:21 +0400

    Updated src path in package

--- tools/setup.py
+++ tools/setup.py
@@ -16,6 +16,7 @@ freezer = Freezer(executables,
     includes = ["lxml._elementpath", "gzip", "inspect"],
     excludes = ["win32api", "win32pipe", "win32con"],
     compress = True,
+    replacePaths = [('..\\src\\', '')],
     base = base)
 freezer.Freeze()
 
