bbf36f1966d6b9102daca498a5763ad3494f2868
Janis <ignatov.vit@gmail.com>
2011-11-03 18:04:02 +0400

    Minor code improvements

--- src/version.py
+++ src/version.py
@@ -3,6 +3,5 @@ from PyQt4 import QtGui
 Company = 'Janis'
 AppName = 'OpenNumismat'
 Version = '0.9'
-# TODO: Fix URL
 Web = 'http://code.google.com/p/open-numismat/'
 AppDir = QtGui.QDesktopServices.storageLocation(QtGui.QDesktopServices.DocumentsLocation) + '/' + AppName
