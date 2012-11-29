203b731ef71e43d737a4f8003c3fb936515eb335
Janis <ignatov.vit@gmail.com>
2012-11-29 13:12:12 +0400

    removed debug output

--- src/Collection/Import/CoinsCollector.py
+++ src/Collection/Import/CoinsCollector.py
@@ -266,7 +266,6 @@ class ImportCoinsCollector(_Import):
                     type_ = imgFields.index(dstColumn)
                     value = pictures[type_]
                     if value:
-                        print(value[:10])
                         image = QtGui.QImage()
                         image.loadFromData(value)
                         record.setValue(dstColumn, image)
