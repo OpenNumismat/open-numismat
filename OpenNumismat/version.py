fe5050b9dd0a88ff5e4587f2039d97f1685c1217
Виталий Игнатов <ignatov.vit@gmail.com>
2017-09-26 11:40:18 +0300

    Version 1.5.4

--- OpenNumismat/version.py
+++ OpenNumismat/version.py
@@ -1,6 +1,6 @@
 Company = 'Janis'
 AppName = 'OpenNumismat'
-Version = '1.5.3'
+Version = '1.5.4'
 Web = 'http://opennumismat.github.io/'
 Author = 'Vitaly Ignatov'
 EMail = 'opennumismat@gmail.com'
