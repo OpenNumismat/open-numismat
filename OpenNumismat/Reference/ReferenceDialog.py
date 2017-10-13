a511e147b455265d9bd17ae25540eef67139ad18
Виталий Игнатов <ignatov.vit@gmail.com>
2017-10-13 12:58:30 +0300

    Remove unused code

--- OpenNumismat/Reference/ReferenceDialog.py
+++ OpenNumismat/Reference/ReferenceDialog.py
@@ -314,34 +314,6 @@ class CrossReferenceDialog(ReferenceDialog):
         return CrossReferenceWidget(section, self.parentIndex, text, self)
 
 
-from functools import WRAPPER_ASSIGNMENTS
-
-
-def class_wraps(cls):
-    """Update a wrapper class `cls` to look like the wrapped."""
-
-    class Wrapper(cls):
-        """New wrapper that will extend the wrapper `cls` to make it look like `wrapped`.
-
-        wrapped: Original function or class that is beign decorated.
-        assigned: A list of attribute to assign to the the wrapper, by default they are:
-             ['__doc__', '__name__', '__module__', '__annotations__'].
-
-        """
-
-        def __init__(self, wrapped, assigned=WRAPPER_ASSIGNMENTS):
-            self.__wrapped = wrapped
-            for attr in assigned:
-                setattr(self, attr, getattr(wrapped, attr))
-
-            super().__init__(wrapped)
-
-        def __repr__(self):
-            return repr(self.__wrapped)
-
-    return Wrapper
-
-
 @storeDlgSizeDecorator
 class AllReferenceDialog(QDialog):
     def __init__(self, reference, parent=None):
