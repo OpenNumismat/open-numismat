d0eb8eee4a0a165ee296a1936ee26b2806e3d7db
Виталий Игнатов <ignatov.vit@gmail.com>
2018-02-07 14:56:26 +0300

    Improve update Statistics

--- OpenNumismat/Collection/VersionUpdater.py
+++ OpenNumismat/Collection/VersionUpdater.py
@@ -389,8 +389,9 @@ class UpdaterTo5(_Updater):
 
             self.collection.fields.userFields.append(fieldDesc)
 
-        sql = "ALTER TABLE statistics ADD COLUMN color INTEGER"
-        QSqlQuery(sql, self.db)
+        if 'statistics' in self.db.tables():
+            sql = "ALTER TABLE statistics ADD COLUMN color INTEGER"
+            QSqlQuery(sql, self.db)
 
         self.progressDlg.setLabelText(self.tr("Saving..."))
 
