2b8149cfcbb733cb3ba99b360e594e977bd73c6c
Виталий Игнатов <ignatov.vit@gmail.com>
2020-06-30 14:50:42 +0300

    OSM engine leaflet 1.6.0

--- OpenNumismat/EditCoinDialog/OSMWidget.py
+++ OpenNumismat/EditCoinDialog/OSMWidget.py
@@ -13,8 +13,8 @@ class OSMWidget(BaseMapWidget):
 <html>
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg==" crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
     <style type="text/css">
         html {
             height: 100%;
