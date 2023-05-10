1c90360c5c164cb42c582daaea0dfa4c78a09050
Janis <opennumismat@gmail.com>
2023-05-10 11:02:31 +0300

    Fix i18n

--- tools/lang_en.ts
+++ tools/lang_en.ts
@@ -264,7 +264,8 @@
         <location filename="ListView.py" line="491"/>
         <source>Are you sure to remove a %n coin(s)?</source>
         <translation>
-            <numerusform></numerusform>
+            <numerusform>Are you sure to remove a %n coin?</numerusform>
+            <numerusform>Are you sure to remove a %n coins?</numerusform>
         </translation>
     </message>
     <message>
@@ -281,7 +282,8 @@
         <location filename="ListView.py" line="158"/>
         <source>%n coin(s) selected</source>
         <translation>
-            <numerusform></numerusform>
+            <numerusform>%n coin selected</numerusform>
+            <numerusform>%n coins selected</numerusform>
         </translation>
     </message>
     <message>
@@ -308,7 +310,7 @@ Close the file and try again.</source>
     </message>
 </context>
 <context>
-    <name>CALENDARS</name>
+    <name>YearCalculatorDialog</name>
     <message>
         <location filename="YearCalculator.py" line="25"/>
         <source>Year calculator</source>
@@ -1124,13 +1126,13 @@ The application will need to restart now.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="SettingsDialog.py" line="248"/>
-        <source>Use relative paths for invoices</source>
+        <location filename="SettingsDialog.py" line="231"/>
+        <source>Convert 0.5 to ½ (support ¼, ⅓, ½, ¾, 1¼, 1½, 2½)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="SettingsDialog.py" line="231"/>
-        <source>Convert 0.5 to &#xc2;&#xbd; (support &#xc2;&#xbc;, &#xe2;&#x85;&#x93;, &#xc2;&#xbd;, &#xc2;&#xbe;, 1&#xc2;&#xbc;, 1&#xc2;&#xbd;, 2&#xc2;&#xbd;)</source>
+        <location filename="SettingsDialog.py" line="248"/>
+        <source>Use relative paths for invoices</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
