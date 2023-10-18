bddc135a79860d2c575bc64577f1584c71df3bc0
Janis <opennumismat@gmail.com>
2023-10-18 12:08:30 +0300

    Import printers for banknotes from Numista

--- OpenNumismat/Collection/Import/Numista.py
+++ OpenNumismat/Collection/Import/Numista.py
@@ -259,6 +259,8 @@ class ImportNumista(_Import2):
             record.setValue('obvrev', item_data['orientation'])
         if 'mints' in item_data:
             record.setValue('mint', item_data['mints'][0]['name'])
+        elif 'printers' in item_data:
+            record.setValue('mint', item_data['printers'][0]['name'])
         if 'min_year' in item_data and 'max_year' in item_data:
             if item_data['min_year'] != item_data['max_year']:
                 record.setValue('dateemis', ' - '.join((str(item_data['min_year']), str(item_data['max_year']))))
