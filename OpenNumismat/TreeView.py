025ab98fda96064f0ef00450ec44d6a5cfce16a7
Janis <opennumismat@gmail.com>
2024-05-17 12:57:30 +0300

    Fix building Tree

--- OpenNumismat/TreeView.py
+++ OpenNumismat/TreeView.py
@@ -29,8 +29,8 @@ class ChildItem():
 class YearTreeWidgetItem(QTreeWidgetItem):
 
     def __lt__(self, other):
-        left = self.data(0, Qt.UserRole + 3)
-        right = other.data(0, Qt.UserRole + 3)
+        left = self.data(0, TreeView.SortDataRole)
+        right = other.data(0, TreeView.SortDataRole)
 
         if not left or not right:
             return super().__lt__(other)
