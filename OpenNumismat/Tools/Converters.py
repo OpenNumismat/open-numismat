649d45df2e6c4c6aca01a2a06050106679473565
Janis <opennumismat@gmail.com>
2024-07-04 16:28:30 +0300

    Fix processing RichText

--- OpenNumismat/Tools/Converters.py
+++ OpenNumismat/Tools/Converters.py
@@ -127,8 +127,10 @@ def numberToFraction(text):
 
 
 def htmlToPlainText(text):
-    RICH_PREFIX = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" '\
-                  '"https://www.w3.org/TR/REC-html40/strict.dtd">'
+    RICH_PREFIX = ('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" '\
+                   '"http://www.w3.org/TR/REC-html40/strict.dtd">',
+                   '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" '\
+                   '"https://www.w3.org/TR/REC-html40/strict.dtd">')
     if text.startswith(RICH_PREFIX):
         document = QTextDocument()
         document.setHtml(text)
