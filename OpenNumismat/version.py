11cf5a2ec55cbfc059d01f72b98bea7065a52391
Janis <opennumismat@gmail.com>
2025-02-11 15:55:47 +0300

    Version 1.10.0

--- OpenNumismat/version.py
+++ OpenNumismat/version.py
@@ -1,6 +1,6 @@
 Company = 'Janis'
 AppName = 'OpenNumismat'
-Version = '1.9.13'
+Version = '1.10.0'
 Web = 'https://opennumismat.github.io/'
 Author = 'Vitaly Ignatov'
 EMail = 'opennumismat@gmail.com'
